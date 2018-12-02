99a72effaa280db3c2b576d8f2f20a7b852afa90
Edward John Steere <edward.steere@gmail.com>
2018-12-02 07:14:23 +0200

    Solve day2

--- day2.el
+++ day2.el
@@ -9,15 +9,62 @@
 
 (require 'cl-lib)
 
+(defun letter-content (str)
+  "Produce a table of the letter count in STR."
+  (let ((table (make-hash-table :test #'eq)))
+    (cl-loop for char across str
+       for cnt = (gethash char table 0)
+       do (puthash char (cl-incf cnt) table)
+       finally return table)))
+
+(require 'subr-x)
+
+(defun contains-three-p (table)
+  "Produce t if TABLE contains the same letter three times."
+  (cl-loop for key in (hash-table-keys table)
+     thereis (eq 3 (gethash key table))))
+
+(defun contains-two-p (table)
+  "Produce t if TABLE contains the same letter three times."
+  (cl-loop for key in (hash-table-keys table)
+     thereis (eq 2 (gethash key table))))
+
 (defun day2-part-1 (input-file)
   "Run my solution to part one of the problem on the input in INPUT-FILE."
-  )
+  (cl-loop for line in (split-string input-file "\n" t " ")
+     with two-count = 0
+     with three-count = 0
+     for content = (letter-content line)
+     when (contains-three-p content)
+       do (cl-incf three-count)
+     when (contains-two-p content)
+       do (cl-incf two-count)
+     finally return (* three-count two-count)))
 
 ;; # PART 2:
 
+(defun differ-by-one-p (this other)
+  "Produce t if THIS is different from OTHER by one character."
+  (cl-loop for i below (length this)
+     with count = 0
+     when (not (eq (aref this i) (aref other i)))
+       do (cl-incf count)
+     finally return (eq 1 count)))
+
 (defun day2-part-2 (input-file)
   "Run my solution to part two of the problem on the input in INPUT-FILE."
-  )
+  (let ((lines (split-string input-file "\n" t " ")))
+    (cl-loop for id in lines
+       with other-string
+       do (cl-loop for other in lines
+             when (differ-by-one-p id other)
+               do (progn
+                    (setq other-string other)
+                    (cl-return)))
+       when other-string
+         return (cl-loop for i below (length id)
+                   when (eq (aref id i) (aref other-string i))
+                     concat (char-to-string (aref id i))))))
 
 ;; Run the solution:
 
