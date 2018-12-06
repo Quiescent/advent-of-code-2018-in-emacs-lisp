6d172a4446049285fae1e591a0022b673fda43fe
Edward John Steere <edward.steere@gmail.com>
2018-12-06 06:46:15 +0200

    Support Linux for continuous running
    Assumes that you have `fswatch' installed.

--- continuously-run.sh
+++ continuously-run.sh
@@ -5,15 +5,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ "$(uname)" == "Darwin" ]; then
-    while [ 1 == 1 ]; do
-        echo "======================================"
-        echo "Restarting watch loop due to errors..."
-        echo "======================================"
-        $(PWD)/runEmacsOnIt.sh $1
-        echo "fswatch -0 \"$(PWD)/$1\" | xargs -0 -n1 -I '{}' $(PWD)/runEmacsOnIt.sh \"{}\""
-        fswatch -0 "$(PWD)/$1" | xargs -0 -n1 -I '{}' $(PWD)/runEmacsOnIt.sh "{}"
-    done
-else
-    echo "TODO: implement this for Linux."
-fi
+while [ 1 == 1 ]; do
+    echo "======================================"
+    echo "Restarting watch loop due to errors..."
+    echo "======================================"
+    $(pwd)/runEmacsOnIt.sh $1
+    echo "fswatch -0 \"$(pwd)/$1\" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh \"{}\""
+    fswatch -0 "$(pwd)/$1" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh "{}"
+done
--- runEmacsOnIt.sh
+++ runEmacsOnIt.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "emacs --batch --eval \"(byte-compile-file \"$1\")\""
-/usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch --eval "(byte-compile-file \"$1\")"
+if [ "$(uname)" == "Darwin" ]; then
+    echo "emacs --batch --eval \"(byte-compile-file \"$1\")\""
+    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch --eval "(byte-compile-file \"$1\")"
 
-echo "emacs --batch -l \"$1c\""
-/usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch -l "$1c"
+    echo "emacs --batch -l \"$1c\""
+    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch -l "$1c"
+else
+    echo "emacs --batch --eval \"(byte-compile-file \"$1\")\""
+    emacs --batch --eval "(byte-compile-file \"$1\")"
+
+    echo "emacs --batch -l \"$1c\""
+    emacs --batch -l "$1c"
+fi
