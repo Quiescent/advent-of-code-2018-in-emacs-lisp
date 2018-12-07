c3694551b4a4c3ea26334826eda499f6047573c4
Edward John Steere <edward.steere@gmail.com>
2018-12-07 08:30:01 +0200

    Improve the runner for Linux

--- continuously-run.sh
+++ continuously-run.sh
@@ -5,11 +5,20 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-while [ 1 == 1 ]; do
-    echo "======================================"
-    echo "Restarting watch loop due to errors..."
-    echo "======================================"
-    $(pwd)/runEmacsOnIt.sh $1
-    echo "fswatch -0 \"$(pwd)/$1\" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh \"{}\""
-    fswatch -0 "$(pwd)/$1" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh "{}"
+if [ "$(uname)" == "Darwin" ]; then
+    while [ 1 == 1 ]; do
+        echo "======================================"
+        echo "Restarting watch loop due to errors..."
+        echo "======================================"
+        $(pwd)/runEmacsOnIt.sh $1
+        echo "fswatch -0 \"$(pwd)/$1\" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh \"{}\""
+        fswatch -0 "$(pwd)/$1" | xargs -0 -n1 -I '{}' $(pwd)/runEmacsOnIt.sh "{}"
+    done
+else
+    inotifywait -e close_write,moved_to,create -m . |
+        while read -r directory events filename; do
+            if [ "$filename" = "$1" ]; then
+                ./runEmacsOnIt.sh "$1"
+            fi
 done
+fi
