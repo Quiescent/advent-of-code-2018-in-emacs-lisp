c6ed7645852c0222e2c018b301d503cc916eb2a1
Edward John Steere <edward.steere@gmail.com>
2018-12-08 10:16:23 +0200

    Setup batch execution for new flag

--- runEmacsOnIt.sh
+++ runEmacsOnIt.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    echo "emacs --batch --eval \"(byte-compile-file \"$1\")\""
-    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch --eval "(byte-compile-file \"$1\")"
+    echo "emacs --batch --no-init -Q --eval \"(byte-compile-file \"$1\")\""
+    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch --no-init -Q --eval "(byte-compile-file \"$1\")"
 
-    echo "emacs --batch -l \"$1c\""
-    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch -l "$1c"
+    echo "emacs --batch --no-init -Q --eval \"(setq run-from-batch t)\" -l \"$1c\""
+    /usr/local/Cellar/emacs-plus/HEAD-2cf9d9f/bin/emacs --batch --no-init -Q --eval "(setq run-from-batch t)" -l "$1c"
 else
-    echo "emacs --batch --eval \"(byte-compile-file \"$1\")\""
-    emacs --batch --eval "(byte-compile-file \"$1\")"
+    echo "emacs --batch --no-init -Q --eval \"(byte-compile-file \"$1\")\""
+    emacs --batch --no-init -Q --eval "(byte-compile-file \"$1\")"
 
-    echo "emacs --batch -l \"$1c\""
-    emacs --batch -l "$1c"
+    echo "emacs --batch --no-init -Q --eval \"(setq run-from-batch t)\" -l \"$1c\""
+    emacs --batch --no-init -Q --eval "(setq run-from-batch t)" -l "$1c"
 fi
+
