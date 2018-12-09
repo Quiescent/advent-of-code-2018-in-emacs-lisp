e2d6fbfe912b95efe844edc8573c5f827bd6cf15
Edward John Steere <edward.steere@gmail.com>
2018-12-09 09:05:47 +0200

    Implement the circular list with a buffer so that it's fast enough
    I hate the fact that Emacs Lisp doesn't have doubly linked lists :/

--- day9.el
+++ day9.el
@@ -21,103 +21,109 @@ so that Emacs doesn't hang.")
     (mapcar #'string-to-number (list (car split)
                                      (nth 6 split)))))
 
-(defun remove-seven-back (marbles len)
-  "Produce the marble in MARBLES which is 7 backwards from current position.
+(defun remove-seven-back ()
+  "Remove and produce the marble in MARBLES which is 7 behind."
+  (let ((count 0))
+    (beginning-of-line)
+    (while (and (not (eq count 7))
+                (not (eq (point) (point-min))))
+      (forward-line -1)
+      (cl-incf count))
+    (when (not (eq count 7))
+      (goto-char (point-max))
+      (forward-line -1)
+      (beginning-of-line)
+      (dotimes (_ (1- (- 7 count)))
+        (forward-line -1)))
+    (prog1
+      (string-to-number (thing-at-point 'symbol t))
+      (kill-line 1))))
 
-LEN is the length of the circular list.
+(with-temp-buffer
+  (mapc (lambda (char) (insert (format "%s\n" char)))
+        '(0 1 2 3 4 5 6 7 8 9))
+  (goto-line 3)
+  (remove-seven-back)
+  (format "At: %s, with values: %s"
+          (line-number-at-pos (point))
+          (buffer-substring (point-min) (point-max))))
 
-Also produce the new list of marbles."
-  (let* ((cons-cell (nthcdr (- len 8) marbles))
-         (marble    (cadr cons-cell))
-         (new-cons  (setcdr cons-cell (cddr cons-cell))))
-    `(,marble ,new-cons)))
+(defun move-forward-twice ()
+  "Move forwards 1 lines in current buffer.
 
-(let ((xs '(2 3 4 5 6 7 8 9 0 1)))
-  (remove-seven-back (setcdr (last xs) xs) 10))
+Rotate to start of buffer when necessary."
+  (when (eq (point) (point-max))
+    (goto-char (point-min)))
+  (forward-line 1)
+  (when (eq (point) (point-max))
+    (goto-char (point-min)))
+  (forward-line 1))
 
-(defun place-marble (current-marble marbles len)
-  "Place the CURRENT-MARBLE into MARBLES at position 2.
-
-LEN is the current length of the list.
-
-MARBLES is assumed to be circular.
-
-Produce the marbles you get to keep (adding them to your score),
-the new list of marbles and the new length of the circular list."
+(defun place-marble (current-marble)
+  "Place the CURRENT-MARBLE into the marbles buffer."
   (if (eq 0 (mod current-marble 23))
-      (pcase (remove-seven-back marbles len)
-        (`(,removed ,marbles-with-removal)
-          (list `(,current-marble ,removed)
-                marbles-with-removal
-                (1- len))))
-      (list '()
-            (progn
-              (setcdr (cdr marbles)
-                      (cons current-marble
-                            (cddr marbles))))
-            (1+ len))))
+      `(,(remove-seven-back) ,current-marble)
+      (progn
+        (move-forward-twice)
+        (save-excursion
+          (insert (format "%s\n" current-marble)))
+        '())))
 
 (require 'seq)
 (require 'subr-x)
 
 (defun day9-part-1 (input-file)
   "Run my solution to part one of the problem on the input in INPUT-FILE."
-  (let ((current-score  nil)
-        (current-marble 1)
-        (current-player 1)
-        (marbles        '(0))
-        (len            1)
-        (player-scores (make-hash-table)))
-    (setcdr marbles marbles)
-    (pcase (parse-settings input-file)
-      (`(,players ,last-score)
-        (while (<= current-marble last-score)
-          (pcase (place-marble current-marble marbles len)
-            (`(,marbles-received
-               ,new-marbles
-               ,new-len)
-              (setq len new-len)
-              (setq marbles new-marbles)
+  (let* ((current-score  nil)
+         (current-marble 1)
+         (current-player 1)
+         (player-scores (make-hash-table)))
+    (with-temp-buffer
+      (insert "0\n")
+      (beginning-of-line)
+      (forward-line -1)
+      (pcase (parse-settings input-file)
+        (`(,players ,last-score)
+          (while (<= current-marble last-score)
+            (let ((marbles-received (place-marble current-marble)))
               (setq current-score (apply #'+ marbles-received))
               (cl-incf (gethash current-player player-scores 0)
-                       current-score)))
-          (setq current-player (mod (1+ current-player) players))
-          (cl-incf current-marble))
-        (seq-max (hash-table-values player-scores))))))
+                       current-score))
+            (setq current-player (mod (1+ current-player) players))
+            (cl-incf current-marble))
+          (seq-max (hash-table-values player-scores)))))))
 
 (let* ((test-input    "10 players; last marble is worth 1618 points")
        (test-computed (day9-part-1 test-input))
        (test-ans      8317))
   (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+
 ;; Solution: 424112
 
 ;; # PART 2:
 
 (defun day9-part-2 (input-file)
   "Run my solution to part two of the problem on the input in INPUT-FILE."
-  (let ((current-score  nil)
-        (current-marble 1)
-        (current-player 1)
-        (marbles        '(0))
-        (len            1)
-        (player-scores (make-hash-table)))
-    (setcdr marbles marbles)
-    (pcase (parse-settings input-file)
-      (`(,players ,last-score)
-        (let ((final-marble (* 100 last-score)))
-          (while (<= current-marble final-marble)
-            (pcase (place-marble current-marble marbles len)
-              (`(,marbles-received
-                 ,new-marbles
-                 ,new-len)
-                (setq len new-len)
-                (setq marbles new-marbles)
-                (setq current-score (apply #'+ marbles-received))
-                (cl-incf (gethash current-player player-scores 0)
-                         current-score)))
+  (let* ((current-score  nil)
+         (current-marble 1)
+         (current-player 1)
+         (player-scores (make-hash-table)))
+    (with-temp-buffer
+      (insert "0\n")
+      (beginning-of-line)
+      (forward-line -1)
+      (pcase (parse-settings input-file)
+        (`(,players ,last-score)
+          (while (<= current-marble (* 100 last-score))
+            (let ((marbles-received (place-marble current-marble)))
+              (setq current-score (apply #'+ marbles-received))
+              (cl-incf (gethash current-player player-scores 0)
+                       current-score))
             (setq current-player (mod (1+ current-player) players))
-            (cl-incf current-marble)))
-        (seq-max (hash-table-values player-scores))))))
+            (cl-incf current-marble))
+          (seq-max (hash-table-values player-scores)))))))
+
+;; Solution: 
 
 ;; Run the solution:
 
