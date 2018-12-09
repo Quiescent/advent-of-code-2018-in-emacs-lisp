97487cef485da6aba046b7cb714c7d6df409cd0f
Edward John Steere <edward.steere@gmail.com>
2018-12-09 06:39:51 +0200

    Add save window excursion to all days

--- day11.el
+++ day11.el
@@ -40,14 +40,16 @@ so that Emacs doesn't hang.")
 (when run-from-batch
   (progn
     (message "\n********** OUTPUT **********")
-    (let ((input-1 (with-temp-buffer
-                     (find-file-literally "day11-part-1")
-                     (buffer-substring (point-min)
-                                       (point-max))))
-          (input-2 (with-temp-buffer
-                     (find-file-literally "day11-part-1")
-                     (buffer-substring (point-min)
-                                       (point-max)))))
+    (let ((input-1 (save-window-excursion
+                     (with-temp-buffer
+                       (find-file-literally "day11-part-1")
+                       (buffer-substring (point-min)
+                                         (point-max)))))
+          (input-2 (save-window-excursion
+                     (with-temp-buffer
+                       (find-file-literally "day11-part-1")
+                       (buffer-substring (point-min)
+                                         (point-max))))))
       (message "Part 1: %s" (day11-part-1 input-1))
       (message "Part 2: %s\n" (day11-part-2 input-2)))))
 
--- day23.el
+++ day23.el
@@ -40,14 +40,16 @@ so that Emacs doesn't hang.")
 (when run-from-batch
   (progn
     (message "\n********** OUTPUT **********")
-    (let ((input-1 (with-temp-buffer
-                     (find-file-literally "day23-part-1")
-                     (buffer-substring (point-min)
-                                       (point-max))))
-          (input-2 (with-temp-buffer
-                     (find-file-literally "day23-part-1")
-                     (buffer-substring (point-min)
-                                       (point-max)))))
+    (let ((input-1 (save-window-excursion
+                     (with-temp-buffer
+                       (find-file-literally "day23-part-1")
+                       (buffer-substring (point-min)
+                                         (point-max)))))
+          (input-2 (save-window-excursion
+                     (with-temp-buffer
+                       (find-file-literally "day23-part-1")
+                       (buffer-substring (point-min)
+                                         (point-max))))))
       (message "Part 1: %s" (day23-part-1 input-1))
       (message "Part 2: %s\n" (day23-part-2 input-2)))))
 
