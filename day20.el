1fbdeb152a66934122078bed129e22110213872d
Edward John Steere <edward.steere@gmail.com>
2018-12-21 01:11:46 +0200

    Solve day20

--- day20.el
+++ day20.el
@@ -17,26 +17,15 @@ so that Emacs doesn't hang.")
 
 (require 'map)
 
-(defun regex-to-graph (regex pos graph x y start-x start-y seen)
-  "Parse REGEX starting from POS and create GRAPH.
-
-Start at X, Y in the GRAPH.
-
-START-X and START-Y are the coords where this subgroup started.
-
-SEEN documents the pos-coord pairs which we've already seen to
-prevent duplicated searches."
-  (cl-loop
-     while (< pos (length regex))
-     with next-x  = 0
-     with next-y  = 0
-     for  char    = (aref regex pos)
-     when (map-elt seen (cons pos (cons x y)))
-       do (progn
-            (message "Seen: %s" (cons pos (cons x y)))
-            (cl-return))
-     do (setf (map-elt seen (cons pos (cons x y))) t)
-     do (message "%s: %s" pos (char-to-string char))
+(defun regex-to-graph (regex graph)
+  "Parse REGEX and create GRAPH."
+  (cl-loop for char being the elements of regex
+     using (index pos)
+     with stack  = '()
+     with next-x = 0
+     with next-y = 0
+     with x      = 0
+     with y      = 0
      do (pcase char
           (?N  (progn
                  (setq next-x x
@@ -48,8 +37,7 @@ prevent duplicated searches."
                        (cl-union (map-elt graph (cons next-x next-y) '())
                                  (list (cons x y))))
                  (setq x next-x
-                       y next-y)
-                 (cl-incf pos)))
+                       y next-y)))
           (?E  (progn
                  (setq next-x (1+ x)
                        next-y y)
@@ -60,8 +48,7 @@ prevent duplicated searches."
                        (cl-union (map-elt graph (cons next-x next-y) '())
                                  (list (cons x y))))
                  (setq x next-x
-                       y next-y)
-                 (cl-incf pos)))
+                       y next-y)))
           (?S  (progn
                  (setq next-x x
                        next-y (1+ y))
@@ -72,8 +59,7 @@ prevent duplicated searches."
                        (cl-union (map-elt graph (cons next-x next-y) '())
                                  (list (cons x y))))
                  (setq x next-x
-                       y next-y)
-                 (cl-incf pos)))
+                       y next-y)))
           (?W  (progn
                  (setq next-x (1- x)
                        next-y y)
@@ -84,59 +70,18 @@ prevent duplicated searches."
                        (cl-union (map-elt graph (cons next-x next-y) '())
                                  (list (cons x y))))
                  (setq x next-x
-                       y next-y)
-                 (cl-incf pos)))
-          (?\( (let* ((pos-and-coords (regex-to-graph regex (1+ pos) graph x y x y seen))
-                      (end-pos        (car pos-and-coords))
-                      (coords         (cdr pos-and-coords))
-                      next-pos
-                      next-coords)
-                 ;; For each coord, continue parsing.
-                 (when (null end-pos)
-                   (cl-return nil))
-                 (cl-return
-                   (cl-loop for coord in coords
-                      for  (x . y)     = coord
-                      do (progn
-                           (let* ((end-pos-and-coords (regex-to-graph regex
-                                                                      end-pos
-                                                                      graph
-                                                                      x
-                                                                      y
-                                                                      start-x
-                                                                      start-y
-                                                                      seen)))
-                             (setq next-pos (car end-pos-and-coords))
-                             (setq next-coords (cl-union next-coords
-                                                         (cdr end-pos-and-coords)))))
-                      finally return (cons next-pos next-coords)))))
-          (?\) (cl-return (cons (1+ pos) (list (cons x y)))))
-          (?|  (progn
-                 ;; Step 1: We start from the starting coord for this
-                 ;; subgroup and continue with the alternate route.
-                 (let* ((end-pos-and-coords (regex-to-graph regex
-                                                            (1+ pos)
-                                                            graph
-                                                            start-x
-                                                            start-y
-                                                            start-x
-                                                            start-y
-                                                            seen))
-                        (pos-end  (car end-pos-and-coords))
-                        (coords   (cdr end-pos-and-coords)))
-                   ;; Step 2: We know where the subgroup ended.  Now
-                   ;; we return the position at the end of the group
-                   ;; along with the coords which the right parse
-                   ;; generated with the current coordinate added.
-                   (when (null pos-end) (cl-return))
-                   (cl-return (cons pos-end
-                                    (cons (cons x y) coords))))))
-          (?$  (progn
-                 (cl-incf pos)
-                 (message "Found end!")))
-          (?^  (cl-incf pos))
-          (`_  (cl-incf pos)))
-     finally return (cons pos (list (cons x y)))))
+                       y next-y)))
+          (?\( (push (cons x y) stack))
+          (?\) (if (eq (aref regex (1- pos)) ?|)
+                   (let ((new-coord (pop stack)))
+                     (setq x (car new-coord))
+                     (setq y (cdr new-coord)))
+                   (pop stack)))
+          (?|  (let ((new-coord (car stack)))
+                 (setq x (car new-coord))
+                 (setq y (cdr new-coord))))
+          (?$  nil)
+          (?^  nil))))
 
 (require 'seq)
 (require 'subr-x)
@@ -173,28 +118,28 @@ prevent duplicated searches."
   (let* ((graph               (make-hash-table :test #'equal))
          (max-lisp-eval-depth 10000)
          (max-specpdl-size    30000))
-    (regex-to-graph input-file 0 graph 0 0 0 0 (make-hash-table :test #'equal))
+    (regex-to-graph input-file graph)
     (longest-path graph)))
 
-;; (let* ((test-input    "^WNE$")
-;;        (test-computed (day20-part-1 test-input))
-;;        (test-ans      3))
-;;   (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+(let* ((test-input    "^WNE$")
+       (test-computed (day20-part-1 test-input))
+       (test-ans      3))
+  (message "Expected: %s\n    Got:      %s" test-ans test-computed))
 
-;; (let* ((test-input    "^ENWWW(NEEE|SSE(EE|N))$")
-;;        (test-computed (day20-part-1 test-input))
-;;        (test-ans      10))
-;;   (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+(let* ((test-input    "^ENWWW(NEEE|SSE(EE|N))$")
+       (test-computed (day20-part-1 test-input))
+       (test-ans      10))
+  (message "Expected: %s\n    Got:      %s" test-ans test-computed))
 
-;; (let* ((test-input    "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$")
-;;        (test-computed (day20-part-1 test-input))
-;;        (test-ans      18))
-;;   (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+(let* ((test-input    "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$")
+       (test-computed (day20-part-1 test-input))
+       (test-ans      18))
+  (message "Expected: %s\n    Got:      %s" test-ans test-computed))
 
-;; (let* ((test-input    "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$")
-;;        (test-computed (day20-part-1 test-input))
-;;        (test-ans      23))
-;;   (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+(let* ((test-input    "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$")
+       (test-computed (day20-part-1 test-input))
+       (test-ans      23))
+  (message "Expected: %s\n    Got:      %s" test-ans test-computed))
 
 (let* ((test-input    "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$")
        (test-computed (day20-part-1 test-input))
@@ -205,14 +150,40 @@ prevent duplicated searches."
 
 ;; # PART 2:
 
+(defun count-over-1000 (graph)
+  "Produce the longest path through GRAPH."
+  (let* ((coord        (cons 0 0))
+         (seen         (make-hash-table :test #'equal))
+         (remaining    (mapcar (lambda (coord) (cons 1 coord))
+                               (progn
+                                 (setf (map-elt seen coord) 1)
+                                 (map-elt graph coord)))))
+    (setf (map-elt seen coord) 0)
+    (cl-loop
+       when (and (not remaining) next-remaining)
+         do (progn
+              (setq remaining      next-remaining
+                    next-remaining '()))
+       while remaining
+       with  next-remaining  = '()
+       for   length-and-next = (pop remaining)
+       for   length          = (car length-and-next)
+       for   next            = (cdr length-and-next)
+       do (cl-loop for destination in (map-elt graph next)
+             when (not (map-contains-key seen destination))
+               do (progn
+                    (setf (map-elt seen destination) (1+ length))
+                    (push (cons (1+ length) destination) next-remaining))))
+    (thread-last (map-values seen)
+      (cl-count-if (lambda (x) (>= x 1000))))))
+
 (defun day20-part-2 (input-file)
   "Run my solution to part two of the problem on the input in INPUT-FILE."
-  )
-
-(let* ((test-input    "")
-       (test-computed (day20-part-2 test-input))
-       (test-ans      0))
-  (message "Expected: %s\n    Got:      %s" test-ans test-computed))
+  (let* ((graph               (make-hash-table :test #'equal))
+         (max-lisp-eval-depth 10000)
+         (max-specpdl-size    30000))
+    (regex-to-graph input-file graph (make-hash-table :test #'equal))
+    (count-over-1000 graph)))
 
 ;; Run the solution:
 
